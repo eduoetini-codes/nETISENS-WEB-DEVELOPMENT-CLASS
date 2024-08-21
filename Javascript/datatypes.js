d81ea5d8fa2958d05186e2e60b792e8baea1fa62
Etini Eduo <eduoetini@gmail.com>
2024-08-21 11:23:41 +0100

    upl

---
+++ Javascript/datatypes.js
@@ -0,0 +1,31 @@
+// Sting variable
+let myName = " Emmanuel Patrick";
+var myCountry = 'USA';
+const myAge = `nine-teen`;
+
+//number variables
+let myNumber = -20;
+let myDecimalNumber = 3.14;
+
+// boolean variables
+let isTrue = true;
+let isFalse = false;
+
+// underfined variables
+let myunderfinedVariable;
+
+// null variables
+let myNullVariable = null;
+
+// array variables
+let myArray = [ 1, " two", isFalse, myName, [3,5.6]];
+
+// bigint variables
+let myBigInt = 1234567890987n;
+
+// object variables
+let myObject = {
+    name: "John Doe",
+    age: 30,
+    city: "Uyo"
+};
