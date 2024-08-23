d6b99ae447582604774d3c31435d2bcf725b7471
Etini Eduo <eduoetini@gmail.com>
2024-08-23 09:09:40 +0100

    upwork

---
+++ Javascript/assignment.js
@@ -0,0 +1,6 @@
+let F;
+let C = 70;
+let cont = 9/5;
+let we = 32;
+F = C * cont + we;
+console.log({F})
---
+++ Javascript/opreators.js
@@ -0,0 +1,33 @@
+// Arithemtic Operators
+// + ( Addition) : a + b
+let num1 = 5;
+let num2 = 9;
+
+console.log(num1 + num2);
+
+// - (subtraction) : a - b
+let num3 = 71;
+let num4 = 14;
+
+console.log(num3 - num4 - num2);
+
+// * (Multiplication): a * b
+console.log(num3 * num1);
+
+// / (Division): a / b
+console.log(num3 / num1);
+
+// % (Modulus); a % b 
+console.log(num4 % num1);
+
+// ++ (Increment): a++ or ++a
+let a = 6;
+console.log({a});
+a++;
+console.log({a});
+
+// -- (Decrement); --a or b--
+let b;
+console.log({b})
+b--
+console.log({b})
